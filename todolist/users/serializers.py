b612caf247fdbb2eea83a2af204e07be74bcae88
ironbee <mazuralexey93@gmail.com>
2021-08-26 16:04:32 +0300

    some reworks

--- todolist/users/serializers.py
+++ todolist/users/serializers.py
@@ -6,6 +6,7 @@ from users.models import CustomUser
 class CustomUserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__' # ?
+        # fields = '__all__' # ?
+        fields = ('username', 'firstname', 'lastname', 'email')
 
 
