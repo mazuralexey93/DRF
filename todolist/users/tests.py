425c6a538cc22ca9acdb84b56b3d9c9705febfc6
ironbee <mazuralexey93@gmail.com>
2021-09-29 00:25:26 +0300

    lesson 8 - APIClient tests

--- todolist/users/tests.py
+++ todolist/users/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
 
+from mainapp.models import Project
 from users.models import CustomUser
 from users.views import CustomUserViewSet
 
@@ -10,13 +11,14 @@ from users.views import CustomUserViewSet
 class TestUserViewSet(TestCase):
 
     def setUp(self):
-        self.superuser = get_user_model().objects.create_superuser(
+        self.admin = get_user_model().objects.create_superuser(
             'admin', 'admin@admin.com', 'admin')
         self.user = get_user_model().objects.create_user(
             'user', 'usermail@mail.om', 'geekbrains')
-        self.project = {'name': 'Prject222', 'link': 'http://localhost/projects/project/222/'}
+        self.project = {'name': 'Project222', 'link': 'http://localhost/projects/project/222/'}
+        self.project_upd = {'name': 'Project111', 'link': 'http://localhost/projects/project/111/'}
 
-    def test_get_list_not_auth(self):
+    def test_get_list_guest(self):
         # 1
         factory = APIRequestFactory()
         request = factory.get('/api/users/')
@@ -66,5 +68,36 @@ class TestUserViewSet(TestCase):
     #     response = view(request)
     #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_get_details_guest(self):
+        # 4
+        project = Project.objects.create(**self.project)
+        client = APIClient()
+        response = client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_details(self):
+        # 5
+        project = Project.objects.create(**self.project)
+        # client = APIClient()
+        client = self.client
+        client.login(username='admin', password='admin')
+        # client.force_authenticate(user=self.user)
+        response = client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # print(response.content)
+        client.logout()
+
+    def test_edit(self):
+        # 5
+        project = Project.objects.create(**self.project)
+        client = APIClient()
+        client.login(username='admin', password='admin')
+        response = client.put(f'/api/projects/{project.id}/', self.project_upd)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        project = Project.objects.get(id=project.id)
+        self.assertEqual(project.name, self.project_upd['name'])
+        self.assertEqual(project.link, self.project_upd['link'])
+        client.logout()
+        # print(response.content)
 
 
