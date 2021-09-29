070fd16da0a2db9185205165737a41b322c2a60a
ironbee <mazuralexey93@gmail.com>
2021-09-29 21:14:10 +0300

    lesson 8 - live test example

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from mainapp.views import ToDoViewSet, ProjectViewSet
 from users.views import CustomUserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+    title="TODO API",
+    permission_classes=[IsAuthenticatedOrReadOnly],
 )
 
 router = DefaultRouter()
@@ -20,8 +27,9 @@ urlpatterns = [
 
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_auth_token),
+    path('api/token-auth/', obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('schema/', schema_view)
 
 ]
--- todolist/users/tests.py
+++ todolist/users/tests.py
@@ -8,93 +8,93 @@ from users.models import CustomUser
 from users.views import CustomUserViewSet
 
 
-class TestUserViewSet(TestCase):
-
-    def setUp(self):
-        self.admin = get_user_model().objects.create_superuser(
-            'admin', 'admin@admin.com', 'admin')
-        self.user = get_user_model().objects.create_user(
-            'user', 'usermail@mail.om', 'geekbrains')
-        self.project = {'name': 'Project222', 'link': 'http://localhost/projects/project/222/'}
-        self.project_upd = {'name': 'Project111', 'link': 'http://localhost/projects/project/111/'}
-
-    def test_get_list_guest(self):
-        # 1
-        factory = APIRequestFactory()
-        request = factory.get('/api/users/')
-        view = CustomUserViewSet.as_view({'get': 'list'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_get_list(self):
-        # 2
-        factory = APIRequestFactory()
-        request = factory.get('/api/users/')
-        view = CustomUserViewSet.as_view({'get': 'list'})
-        force_authenticate(request, user=self.user)
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_create_guest(self):
-        """
-        !!! 'CustomUserViewSet' object has no attribute 'create'
-        Переопределил  def create(self):
-        super(CustomUserViewSet, self).create()
-        """
-        # 3
-        data = {'first_name': 'Egor',
-                'last_name': 'Sechin',
-                'email': 'EES@locla.om'}
-
-        factory = APIRequestFactory()
-        request = factory.post('/api/users/', data)
-        view = CustomUserViewSet.as_view({'post': 'create'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    # def test_create_admin(self):
-    #     """
-    # TypeError: create() takes 0 positional arguments but 2 were given
-    # ???
-    #     """
-    #     data = {'first_name': 'Egor',
-    #             'last_name': 'Sechin',
-    #             'email': 'EES@locla.om'}
-    #
-    #     factory = APIRequestFactory()
-    #     request = factory.post('/api/users/', data)
-    #     view = CustomUserViewSet.as_view({'post': 'create'})
-    #     force_authenticate(request, user=self.superuser)
-    #     response = view(request)
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_get_details_guest(self):
-        # 4
-        project = Project.objects.create(**self.project)
-        client = APIClient()
-        response = client.get(f'/api/projects/{project.id}/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_get_details(self):
-        # 5
-        project = Project.objects.create(**self.project)
-        client = APIClient()
-        client.force_authenticate(user=self.user)
-        response = client.get(f'/api/projects/{project.id}/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # print(response.content)
-        client.logout()
-
-    def test_edit(self):
-        # 6
-        project = Project.objects.create(**self.project)
-        client = APIClient()
-        client.login(username='admin', password='admin')
-        response = client.put(f'/api/projects/{project.id}/',
-                              self.project_upd)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        project = Project.objects.get(id=project.id)
-        self.assertEqual(project.name, self.project_upd['name'])
-        self.assertEqual(project.link, self.project_upd['link'])
-        client.logout()
-        # print(response.content)
+# class TestUserViewSet(TestCase):
+#
+#     def setUp(self):
+#         self.admin = get_user_model().objects.create_superuser(
+#             'admin', 'admin@admin.com', 'admin')
+#         self.user = get_user_model().objects.create_user(
+#             'user', 'usermail@mail.om', 'geekbrains')
+#         self.project = {'name': 'Project222', 'link': 'http://localhost/projects/project/222/'}
+#         self.project_upd = {'name': 'Project111', 'link': 'http://localhost/projects/project/111/'}
+#
+#     def test_get_list_guest(self):
+#         # 1
+#         factory = APIRequestFactory()
+#         request = factory.get('/api/users/')
+#         view = CustomUserViewSet.as_view({'get': 'list'})
+#         response = view(request)
+#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#
+#     def test_get_list(self):
+#         # 2
+#         factory = APIRequestFactory()
+#         request = factory.get('/api/users/')
+#         view = CustomUserViewSet.as_view({'get': 'list'})
+#         force_authenticate(request, user=self.user)
+#         response = view(request)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     def test_create_guest(self):
+#         """
+#         !!! 'CustomUserViewSet' object has no attribute 'create'
+#         Переопределил  def create(self):
+#         super(CustomUserViewSet, self).create()
+#         """
+#         # 3
+#         data = {'first_name': 'Egor',
+#                 'last_name': 'Sechin',
+#                 'email': 'EES@locla.om'}
+#
+#         factory = APIRequestFactory()
+#         request = factory.post('/api/users/', data)
+#         view = CustomUserViewSet.as_view({'post': 'create'})
+#         response = view(request)
+#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#
+#     # def test_create_admin(self):
+#     #     """
+#     # TypeError: create() takes 0 positional arguments but 2 were given
+#     # ???
+#     #     """
+#     #     data = {'first_name': 'Egor',
+#     #             'last_name': 'Sechin',
+#     #             'email': 'EES@locla.om'}
+#     #
+#     #     factory = APIRequestFactory()
+#     #     request = factory.post('/api/users/', data)
+#     #     view = CustomUserViewSet.as_view({'post': 'create'})
+#     #     force_authenticate(request, user=self.superuser)
+#     #     response = view(request)
+#     #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#
+#     def test_get_details_guest(self):
+#         # 4
+#         project = Project.objects.create(**self.project)
+#         client = APIClient()
+#         response = client.get(f'/api/projects/{project.id}/')
+#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+#
+#     def test_get_details(self):
+#         # 5
+#         project = Project.objects.create(**self.project)
+#         client = APIClient()
+#         client.force_authenticate(user=self.user)
+#         response = client.get(f'/api/projects/{project.id}/')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         # print(response.content)
+#         client.logout()
+#
+#     def test_edit(self):
+#         # 6
+#         project = Project.objects.create(**self.project)
+#         client = APIClient()
+#         client.login(username='admin', password='admin')
+#         response = client.put(f'/api/projects/{project.id}/',
+#                               self.project_upd)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#         project = Project.objects.get(id=project.id)
+#         self.assertEqual(project.name, self.project_upd['name'])
+#         self.assertEqual(project.link, self.project_upd['link'])
+#         client.logout()
+#         # print(response.content)
