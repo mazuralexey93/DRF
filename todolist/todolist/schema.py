7c3dfac928a1b10517b358f4f63fbb10b8dda215
ironbee <mazuralexey93@gmail.com>
2021-10-08 19:01:55 +0300

    lesson 10 - mutation example

--- todolist/todolist/schema.py
+++ todolist/todolist/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene import Mutation
 from graphene_django import DjangoObjectType
 from mainapp.models import Project, ToDo
 from users.models import CustomUser
@@ -51,4 +52,23 @@ class Query(graphene.ObjectType):
         return todos
 
 
-schema = graphene.Schema(query=Query)
+class ProjectMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        id = graphene.ID()
+
+    project = graphene.Field(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        project = Project.objects.get(pk=id)
+        project.name = name
+        project.save()
+        return ProjectMutation(project=project)
+
+
+class Mutation(graphene.ObjectType):
+    update_project = ProjectMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
